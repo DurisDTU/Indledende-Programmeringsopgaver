3d752af977ddb5d3ec688e57c38640e985cf75c5
Nicki <Nicki@10.16.74.48>
2015-11-11 11:00:23 +0100

    Opg5 almost done

--- 2Opgave/Opgave5/Line.java
+++ 2Opgave/Opgave5/Line.java
@@ -5,15 +5,21 @@ public class Line {
 
 	public static void main(String[] args){
 
-		Scanner in = new Scanner(System.in);
+		int a = 2;
+		int b = 10;
 		int førsteKoordinat;
 		int andetKoordinat;
+		String l = "y = a*x + b";
+		
+		Scanner in = new Scanner(System.in);
 		System.out.print("Indtast første koordinat.");
 		førsteKoordinat = in.nextInt();
 		System.out.print("Indtast andet koordinat.");
 		andetKoordinat = in.nextInt();
 		
 		System.out.println("Koordinaterne for linjen er: " + førsteKoordinat +","+andetKoordinat);
+		
+		System.out.println("Længden på linjen er :");
 		
 		
 	}
